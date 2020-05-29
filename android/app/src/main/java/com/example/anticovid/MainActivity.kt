8e458d4bcaf917408b71bec60671c04c9406eed2
Chuckhu18 <chuckhu301@yahoo.com>
2020-05-29 15:08:39 -0700

    rough draft

--- android/app/src/main/java/com/example/anticovid/MainActivity.kt
+++ android/app/src/main/java/com/example/anticovid/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.anticovid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun send_second(view: View){
+        val intent = Intent(this, SecondActivity::class.java)
+        startActivity(intent)
     }
 }
