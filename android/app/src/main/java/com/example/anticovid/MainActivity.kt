e2b4a66dbf86596011f75445b2630a35eaa97ebf
DDMJuz <DDM@DDM--BBG.local>
2020-05-31 09:51:42 -0700

    connected to DB, added timestamp as sorting factor

--- android/app/src/main/java/com/example/anticovid/MainActivity.kt
+++ android/app/src/main/java/com/example/anticovid/MainActivity.kt
@@ -1,21 +1,27 @@
 package com.example.anticovid
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_main.*
+import java.sql.Timestamp
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
     var emailEditText: EditText? = null
     var passwordEditText: EditText? = null
+    var nameEditText: EditText? = null
     val mAuth = FirebaseAuth.getInstance()
+    var rightNow: Calendar = Calendar.getInstance()
+    var timestamp: Timestamp = Timestamp(System.currentTimeMillis())
     private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,11 +29,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         // Initialize Firebase Auth
         auth = Firebase.auth
+        nameEditText = findViewById(R.id.nameEditText)
         emailEditText = findViewById(R.id.emailEditText)
         passwordEditText = findViewById(R.id.passwordEditText)
 
         if(mAuth.currentUser != null){
-
+            logIn()
         }
     }
 
@@ -48,6 +55,12 @@ class MainActivity : AppCompatActivity() {
                         .addOnCompleteListener(this){task ->
                             if (task.isSuccessful){
                                 //Add to DB
+                                Firebase.database.getReference().child("patients").child(nameEditText?.text.toString()).child(task.result?.user!!.uid)
+                                Firebase.database.getReference().child("patients").child(nameEditText?.text.toString()).child("email").setValue(emailEditText?.text.toString())
+                                Firebase.database.getReference().child("patients").child(nameEditText?.text.toString()).child("Time").setValue(rightNow)
+                                Firebase.database.getReference().child("patients").child(nameEditText?.text.toString()).child("Timestamp").setValue(timestamp.time)
+
+                                //Login the user
                                 logIn()
                             }else{
                                 Toast.makeText(baseContext, "Authentication failed. Try Again",
