5f864a1120ac438b830268b902c0bf4f1cef5fd6
changer666666 <30694064+changer666666@users.noreply.github.com>
2020-06-01 17:56:05 -0700

    server update

--- server/server.php
+++ server/server.php
@@ -1,9 +1,47 @@
 <?php
+	require __DIR__.'/vendor/autoload.php';
+	use Kreait\Firebase\Factory;
 
-	$queue = new SplQueue();
-	$queue->enqueue('A Patient');
-	$queue->enqueue('B Patient');
-	$queue->enqueue('C Patient');
+	$factory = (new Factory())->withDatabaseUri('https://anti-covid-5e9df.firebaseio.com');
+
+	// $factory = (new Factory())->withDatabaseUri('https://anticovid-a242d.firebaseio.com');
+
+	$database = $factory->createDatabase();
+
+	$reference = $database->getReference('/users/2');
+	// $reference = $database->getReference('/patients');
+	// $reference = $database->getReference('/Waiting List');
+
+	//set or replace value in database
+	// $reference
+ //    ->set([
+ //       // 'name' => 'a2',
+ //       // 'emails' => 'salesa2@domain.tld',
+ //       // 'website' => 'https://app.domain.tlda2',
+ //      ]);
+
+	//query values in database
+	$snapshot = $reference->orderByChild('name')->limitToFirst(1)->getSnapshot()->remove();
+	$snapshotAll = $reference->orderByChild('name')->getSnapshot();
+	$value = $snapshot->getValue();
+	$valueAll = $snapshotAll->getValue();
+
+	// $queue = new SplQueue();
+	// $queue->enqueue('A Patient');
+	// $queue->enqueue('B Patient');
+	// $queue->enqueue('C Patient');
+	echo ("<pre>");
+	print_r($value);
+	echo ("</pre>");
+
+
+	$firebase->getAuth()->deleteUser('4');
+
+	// $key = array_search($value, (array)$valueAll);
+	// print_r($key);
+
+	// $child = $snapshot->hasChild();
+	// print_r($child);
 
 
 	$name = "";
@@ -13,13 +51,11 @@
 	$doctorName = "";
 	$doctorInfo = "";
 
-
 	if ( isset($_GET["name"]) ) {
 		$name = $_GET["name"];
 		$DOB = $_GET["DOB"];
 		$info = $_GET["info"];
 		$ip = $_SERVER["REMOTE_ADDR"];
-
 
 
 		echo "<br>name is: $name";
@@ -29,8 +65,6 @@
 
 		//enqueue patient
 		//invoke database, add one patient(id increment), save android ip
-
-
 
 	}
 
