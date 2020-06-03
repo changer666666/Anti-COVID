7db22d3ec7c5f2c8b9522d1dd16b68ea2243190e
Chuckhu18 <chuckhu301@yahoo.com>
2020-06-02 17:48:58 -0700

    bluetooth setup done

--- android/app/src/main/java/com/example/anticovid/ThirdActivity.kt
+++ android/app/src/main/java/com/example/anticovid/ThirdActivity.kt
@@ -1,23 +1,33 @@
 package com.example.anticovid
 
+import android.Manifest
 import android.content.Intent
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothGattCharacteristic
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
 import android.util.Log
 import android.view.View
-import android.widget.Button
-import android.widget.RadioButton
-import android.widget.RadioGroup
-import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.core.app.ActivityCompat
+import android.widget.TextView
 import kotlinx.android.synthetic.main.third_activity.*
 
-class ThirdActivity: AppCompatActivity() {
+const val DEVICE_NAME = "bluet"
+
+class ThirdActivity: AppCompatActivity(), BLEControl.Callback {
+
+    // Bluetooth
+    private var ble: BLEControl? = null
+    private var rssiAverage:Double = 0.0
+    private var messages: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.third_activity)
 
+        //Summary View Text
         var passed = ArrayList<String>()
         var textshow = "Summary:"
 
@@ -25,8 +35,120 @@ class ThirdActivity: AppCompatActivity() {
         for( i in passed){
             textshow += "\n" + i
         }
-
 //        summaryTextView.text = textshow
+
+        // Ensures Bluetooth is available on the device and it is enabled. If not,
+        // displays a dialog requesting user permission to enable Bluetooth.
+        val adapter: BluetoothAdapter?
+        adapter = BluetoothAdapter.getDefaultAdapter()
+        if (adapter != null) {
+            if (!adapter.isEnabled) {
+                val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
+
+            }        }
+
+        // Get Bluetooth
+        messages = findViewById(R.id.bluetoothText)
+        messages!!.movementMethod = ScrollingMovementMethod()
+        ble = BLEControl(applicationContext, DEVICE_NAME)
+
+        // Check permissions
+        ActivityCompat.requestPermissions(this,
+            arrayOf( Manifest.permission.BLUETOOTH, Manifest.permission.BLUETOOTH_ADMIN, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION), 1)
+
+    }
+
+    override fun onRSSIread(uart:BLEControl,rssi:Int){
+        rssiAverage = rssi.toDouble()
+        writeLine("RSSI $rssiAverage")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        //updateButtons(false)
+        ble!!.registerCallback(this)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        ble!!.unregisterCallback(this)
+        ble!!.disconnect()
+    }
+
+    fun connect(v: View) {
+        startScan()
+    }
+
+    private fun startScan() {
+        writeLine("Scanning for devices ...")
+        ble!!.connectFirstAvailable()
+    }
+
+    /**
+     * Writes a line to the messages textbox
+     * @param text: the text that you want to write
+     */
+    private fun writeLine(text: CharSequence) {
+        runOnUiThread {
+            messages!!.append(text)
+            messages!!.append("\n")
+        }
+    }
+
+    /**
+     * Called when a UART device is discovered (after calling startScan)
+     * @param device: the BLE device
+     */
+    override fun onDeviceFound(device: BluetoothDevice) {
+        writeLine("Found device : " + device.name)
+        writeLine("Waiting for a connection ...")
+    }
+
+    /**
+     * Prints the devices information
+     */
+    override fun onDeviceInfoAvailable() {
+        writeLine(ble!!.deviceInfo)
+    }
+
+    /**
+     * Called when UART device is connected and ready to send/receive data
+     * @param ble: the BLE UART object
+     */
+    override fun onConnected(ble: BLEControl) {
+        writeLine("Connected!")
+
+    }
+
+    /**
+     * Called when some error occurred which prevented UART connection from completing
+     * @param ble: the BLE UART object
+     */
+    override fun onConnectFailed(ble: BLEControl) {
+        writeLine("Error connecting to device!")
+    }
+
+    /**
+     * Called when the UART device disconnected
+     * @param ble: the BLE UART object
+     */
+    override fun onDisconnected(ble: BLEControl) {
+        writeLine("Disconnected!")
+    }
+
+    /**
+     * Called when data is received by the UART
+     * @param ble: the BLE UART object
+     * @param rx: the received characteristic
+     */
+    override fun onReceive(ble: BLEControl, rx: BluetoothGattCharacteristic) {
+        writeLine("Received value: " + rx.getStringValue(0))
+
+    }
+
+    companion object {
+        private val REQUEST_ENABLE_BT = 0
     }
 
 }
